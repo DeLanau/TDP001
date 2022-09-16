6298cf1ed898257b9678bbd4c516e1fb771328c3
Nadim Lakrouz <nadla777@student.liu.se>
2022-09-16 13:35:49 +0200

    upg3

---
+++ regex.py
@@ -0,0 +1,28 @@
+import urllib.request
+import re
+
+#hämta url och decode i utf-8
+
+with urllib.request.urlopen('https://php-web.edu.liu.se/nadla777/TDP001/') as response:
+   html = response.read().decode("utf-8")
+
+
+#hitta alla tags som passar regex 
+#Regex [^/!] börja från första tecken tills slut  \S* för att hämta allt inne i <> eller fran < till slut av första tag.
+
+match = re.findall("(<[^/!]\S*>|<[^/!]\S*)", html)
+
+#fixa taggar sasom <a ....
+
+for i, tag in enumerate(match):
+    if not tag.endswith(">"):
+        match[i] = tag + ">"
+
+#count och utskrift av taggar
+
+for tag in match:
+    tag_count = match.count(tag)
+    print(str(tag_count) + " " + tag)
+    if tag_count > 1:
+        for i in range(0, tag_count):
+            match.remove(tag)
